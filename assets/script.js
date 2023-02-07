9b6ee70ff9b17a0a55110050cacb3d3fade008b2
fahim-86 <fam.fahim@yahoo.com>
2023-02-07 20:39:15 +1100

    Fix ESlinter errors

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,14 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelector('#toggleMenuButton').addEventListener('click', () => {
-        console.log();
-        document.querySelector('.pf-header').classList.toggle('toggled');
-        document.querySelector('#toggleMenuItems').toggleAttribute('hidden');
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#toggleMenuButton').addEventListener('click', () => {
+    document.querySelector('.pf-header').classList.toggle('toggled');
+    document.querySelector('#toggleMenuItems').toggleAttribute('hidden');
+  });
+  document.querySelector('#x-icon').addEventListener('click', () => {
+    document.querySelector('#toggleMenuItems').toggleAttribute('hidden');
+  });
+  document.querySelectorAll('.coll-nav-items li').forEach((item) => {
+    item.addEventListener('click', () => {
+      document.querySelector('#toggleMenuItems').toggleAttribute('hidden');
     });
-    document.querySelector('#x-icon').addEventListener('click', () => {
-        document.querySelector('#toggleMenuItems').toggleAttribute('hidden');
-    });
-    document.querySelectorAll('.coll-nav-items li').forEach((item) => {
-        item.addEventListener('click', () => {
-        document.querySelector('#toggleMenuItems').toggleAttribute('hidden');
-      })
-    })
-})
+  });
+});
