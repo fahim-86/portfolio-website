4bf35986a9727f3744d585f7fbcf25a3edc15167
fahim-86 <fam.fahim@yahoo.com>
2023-02-10 08:25:26 +1100

    Popup page ready for desktop view

--- assets/popup.js
+++ assets/popup.js
@@ -1,23 +1,20 @@
 const modal = document.querySelector('.pop-modal');
 const innerModal = document.querySelector('.pop-inner-modal');
 const cancelModal = document.querySelector('.cancel');
-const works = document.querySelector('#portfolioSection');
+// const works = document.querySelector('#portfolio-popup');
 const width = window.innerWidth;
-const isDesktop = width > 426;
-const isMobile = width > 320 && width < 425;
+const isDesktop = width > 320;
+// const isMobile = width > 320 && width < 425;
 
 const cards = [
   {
     id: 0,
     name: 'Tonic',
-    mainDescription:
-        'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
     desktopDescription:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    featuredImage: './images/Snapshoot1.png',
-    desktopFeaturedImage: './images/Snap.png',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    desktopFeaturedImage: './img/tonic-2.png',
     technologies: [
       'html',
       'css',
@@ -28,20 +25,17 @@ const cards = [
     ],
     stack: ['CANOPY', 'Back End Dev', 2015],
     mobileStack: ['CANOPY', 'Back End Dev', 2015],
-    linkToLiveVersion: 'https://moses-chibuike.github.io/Portfolio-microverse/',
-    linkToSource: 'https://github.com/Moses-chibuike/Portfolio-microverse',
+    linkToLiveVersion: 'https://fahim-86.github.io/portfolio-website/',
+    linkToSource: 'https://github.com/fahim-86/portfolio-website',
   },
   {
     id: 1,
     name: 'Multi-Post Stories',
-    mainDescription:
-        'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
     desktopDescription:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    featuredImage: './images/Snapshoot2.png',
-    desktopFeaturedImage: './images/Snap2.png',
+    desktopFeaturedImage: './img/mp-stories2.png',
     technologies: [
       'html',
       'css',
@@ -58,14 +52,11 @@ const cards = [
   {
     id: 2,
     name: 'Facebook 360',
-    mainDescription:
-        "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     desktopDescription:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    featuredImage: './images/Snapshoot3.png',
-    desktopFeaturedImage: './images/Snap3.png',
+    desktopFeaturedImage: './img/tonic.png',
     technologies: [
       'html',
       'css',
@@ -82,14 +73,11 @@ const cards = [
   {
     id: 3,
     name: 'Uber Navigation',
-    mainDescription:
-        'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     desktopDescription:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    featuredImage: './images/Snapshoot4.png',
-    desktopFeaturedImage: './images/Snap4.png',
+    desktopFeaturedImage: './img/mp-stories.png',
     technologies: [
       'html',
       'css',
@@ -105,152 +93,134 @@ const cards = [
   },
 ];
 
-const openModal = ({
-  name,
-  description,
-  desktopDescription,
-  featuredImage,
-  desktopFeaturedImage,
-  technologies,
-  stack,
-  mobileStack,
-  linkToLiveVersion,
-  linkToSource,
-}) => {
-  let display = '';
-  modal.classList.add('showModal');
-  display += `
-  <div>
-  <h2 class="modal-h2">${name}</h2>
-  <ul class="flex item-tag-modal item-tag">
-    
-    ${
-  isDesktop
-    ? `<li class="icon-tag"><a class="canopy" href="#">${stack[0]}</a></li>`
-    : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[0]}</a></li>`
-}
-    <li class="radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
-    ${
-  isDesktop
-    ? `<li class="icon-tag"><a class="bend-yr bend" href="#">${stack[1]}</a></li>`
-    : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[1]}</a></li>`
-}
-    <li class=" radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
-    ${
-  isDesktop
-    ? `<li class="icon-tag"><a class="bend-yr"href="#">${stack[2]}</a></li>`
-    : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[2]}</a></li>`
-}
-  </ul>
-  <div class="image img-modal ">
-    <img src=${isDesktop ? desktopFeaturedImage : featuredImage} alt="Project"/>
-  </div>
-  <div class="flex-modal">
-    <p class="p p-modal">
-     ${isDesktop ? desktopDescription : description}
-    </p>
-    
-    <div class="langbutton langbutton-modal">
-    <div class= "modal-btn">
-      <button>${technologies[0]}</button>
-      <button>${technologies[1]}</button>
-      <button>${technologies[2]}</button>
-      ${isDesktop ? `<button>${technologies[3]}</button>` : ''}
-      ${isDesktop ? `<button>${technologies[4]}</button>` : ''}
-      ${isDesktop ? `<button>${technologies[5]}</button>` : ''}
-    </div>
-      <hr class="modal-new">
-      
-      <div class="popup-buttons flex div">
-      
-      <a href=${linkToLiveVersion} target= "_blank" class="btn-modal btn">See Live <img src="./images/IconModal.png" alt="Live Icon " /></a>
-        <a href=${linkToSource} target= "_blank" class="btn-modal btn">See Source <img src="./images/VectorModal.png" alt="Source Icon" /></a>
-    </div>
-    
-    </div>
-    </div>
-</div>
-</section>`;
+const loadWorks = (data, btnID) => {
+  let display = ' ';
+  console.log(data);
+  console.log('This is' + btnID);
+  if (btnID === 'loadModal') {
+    display += `<div class="card-text">
+      <h2>${data[0].name}</h2>
+      <div class="under-header-text">
+        <h4>${data[0].mobileStack[0]}</h4>
+        <ul>
+          <li>${data[0].mobileStack[1]}</li>
+          <li>${data[0].mobileStack[2]}</li>
+        </ul>
+      </div>
+      <img class="pop-img" src="${data[0].desktopFeaturedImage}" alt="Multi-post Stories" />
+      <div class="pop-txt-btn">
+        <p>${data[0].desktopDescription}</p>
+        <ul class="buttons">
+          <li class="btn">${data[0].technologies[0]}</li>
+          <li class="btn">${data[0].technologies[1]}</li>
+          <li class="btn">${data[0].technologies[2]}</li>
+          <li class="btn">${data[0].technologies[3]}</li>
+          <li class="btn">${data[0].technologies[4]}</li>
+          <li class="btn">${data[0].technologies[5]}</li>
+          <div class="pop-lrg-btn"><a href="${data[0].linkToLiveVersion}" class="btn-large" target="_blank">See Live<img class="" src="./img/live-btn-Icon.png" alt="Icon btn live" /></a><a href="${data[0].linkToSource}" class="btn-large" target="_blank">See Source<img class="" src="./img/source-btn-icon.png" alt="Icon source btn" /></a></div>
+        </ul>
+      </div>
+    </div>`;
+  } else if (btnID === 'loadModal2') {
+    display += `<div class="card-text">
+      <h2>${data[1].name}</h2>
+      <div class="under-header-text">
+        <h4>${data[1].mobileStack[0]}</h4>
+        <ul>
+          <li>${data[1].mobileStack[1]}</li>
+          <li>${data[1].mobileStack[2]}</li>
+        </ul>
+      </div>
+      <img class="pop-img" src="${data[1].desktopFeaturedImage}" alt="Multi-post Stories" />
+      <div class="pop-txt-btn">
+        <p>${data[1].desktopDescription}</p>
+        <ul class="buttons">
+          <li class="btn">${data[1].technologies[0]}</li>
+          <li class="btn">${data[1].technologies[1]}</li>
+          <li class="btn">${data[1].technologies[2]}</li>
+          <li class="btn">${data[1].technologies[3]}</li>
+          <li class="btn">${data[1].technologies[4]}</li>
+          <li class="btn">${data[1].technologies[5]}</li>
+          <div class="pop-lrg-btn"><a href="${data[1].linkToLiveVersion}" class="btn-large" target="_blank">See Live<img class="" src="./img/live-btn-Icon.png" alt="Icon btn live" /></a><a href="${data[1].linkToSource}" class="btn-large" target="_blank">See Source<img class="" src="./img/source-btn-icon.png" alt="Icon source btn" /></a></div>
+        </ul>
+      </div>
+    </div>`;
+  } else if (btnID === 'loadModal3') {
+    display += `<div class="card-text">
+      <h2>${data[2].name}</h2>
+      <div class="under-header-text">
+        <h4>${data[2].mobileStack[0]}</h4>
+        <ul>
+          <li>${data[2].mobileStack[1]}</li>
+          <li>${data[2].mobileStack[2]}</li>
+        </ul>
+      </div>
+      <img class="pop-img" src="${data[2].desktopFeaturedImage}" alt="Multi-post Stories" />
+      <div class="pop-txt-btn">
+        <p>${data[2].desktopDescription}</p>
+        <ul class="buttons">
+          <li class="btn">${data[2].technologies[0]}</li>
+          <li class="btn">${data[2].technologies[1]}</li>
+          <li class="btn">${data[2].technologies[2]}</li>
+          <li class="btn">${data[2].technologies[3]}</li>
+          <li class="btn">${data[2].technologies[4]}</li>
+          <li class="btn">${data[2].technologies[5]}</li>
+          <div class="pop-lrg-btn"><a href="${data[2].linkToLiveVersion}" class="btn-large" target="_blank">See Live<img class="" src="./img/live-btn-Icon.png" alt="Icon btn live" /></a><a href="${data[2].linkToSource}" class="btn-large" target="_blank">See Source<img class="" src="./img/source-btn-icon.png" alt="Icon source btn" /></a></div>
+        </ul>
+      </div>
+    </div>`;
+  } else if (btnID === 'loadModal4') {
+    display += `<div class="card-text">
+      <h2>${data[3].name}</h2>
+      <div class="under-header-text">
+        <h4>${data[3].mobileStack[0]}</h4>
+        <ul>
+          <li>${data[3].mobileStack[1]}</li>
+          <li>${data[3].mobileStack[2]}</li>
+        </ul>
+      </div>
+      <img class="pop-img" src="${data[3].desktopFeaturedImage}" alt="Multi-post Stories" />
+      <div class="pop-txt-btn">
+        <p>${data[3].desktopDescription}</p>
+        <ul class="buttons">
+          <li class="btn">${data[3].technologies[0]}</li>
+          <li class="btn">${data[3].technologies[1]}</li>
+          <li class="btn">${data[3].technologies[2]}</li>
+          <li class="btn">${data[3].technologies[3]}</li>
+          <li class="btn">${data[3].technologies[4]}</li>
+          <li class="btn">${data[3].technologies[5]}</li>
+          <div class="pop-lrg-btn"><a href="${data[3].linkToLiveVersion}" class="btn-large" target="_blank">See Live<img class="" src="./img/live-btn-Icon.png" alt="Icon btn live" /></a><a href="${data[3].linkToSource}" class="btn-large" target="_blank">See Source<img class="" src="./img/source-btn-icon.png" alt="Icon source btn" /></a></div>
+        </ul>
+      </div>
+    </div>`;
+  }
   innerModal.innerHTML = display;
-};
-
-const loadModal = (i) => {
-  const singleData = cards.find((item) => item.id === i);
-  openModal(singleData);
 };
 
 cancelModal.addEventListener('click', () => {
   modal.classList.remove('showModal');
 });
 
-const loadWorks = (data) => {
-  let display = ' ';
-  data.forEach(
-    (
-      {
-        name,
-        mainDescription,
-        featuredImage,
-        desktopFeaturedImage,
-        technologies,
-        stack,
-        mobileStack,
-      },
-      i,
-    ) => {
-      display += `
-      <div class="card-works works">
-      <div class="image ">
-        <img class="desktop-image" src= ${
-  isDesktop ? desktopFeaturedImage : featuredImage
-} alt="Project"/>
-      </div>
-      <div class="card">
-        <h2>${name}</h2>
-        <ul class="flex item-tag">
-          ${
-  isDesktop
-    ? `<li class="icon-tag"><a class="canopy" href="#">${stack[0]}</a></li>`
-    : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[0]}</a></li>`
-}
-          <li class="radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
-          ${
-  isDesktop
-    ? `<li class="icon-tag"><a class="bend-yr bend" href="#">${stack[1]}</a></li>`
-    : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[1]}</a></li>`
-}
-          <li class=" radio icon-tag"><img src="images/Counter.png" alt="counter" /></li>
-          ${
-  isDesktop
-    ? `<li class="icon-tag"><a class="bend-yr"href="#">${stack[2]}</a></li>`
-    : `<li class="icon-tag"><a class="canopy" href="#">${mobileStack[2]}</a></li>`
-}
-        </ul>
-        <p class="p">
-          ${mainDescription}
-        </p>
-        <div class="langbutton">
-          <button>${technologies[0]}</button>
-          <button>${technologies[4]}</button>
-          <button>${technologies[1]}</button>
-          ${isDesktop ? `<button>${technologies[2]}</button>` : ''}
-        </div>
-  
-        <div class="div">
-        <button type="submit" id="loadModal" value="${i}" onclick='loadModal(${i}) '  class="btn-modal btn">See Project</button>
-        </div>
-        </div>
-      </div> `;
-    },
-  );
-  works.innerHTML = display;
-};
-
 window.addEventListener('DOMContentLoaded', () => {
-  loadWorks(cards);
-
-  const loadModalEle = document.querySelector('#loadModal');
-  loadModalEle.addEventListener('click', () => {
-    loadModal(loadModalEle.value);
+  document.querySelector('#loadModal').addEventListener('click', (e) => {
+    // console.log(e.target.id);
+    const btnID = e.target.id;
+    modal.classList.add('showModal');
+    loadWorks(cards, btnID);
+  });
+  document.querySelector('#loadModal2').addEventListener('click', (e) => {
+    const btnID = e.target.id;
+    modal.classList.add('showModal');
+    loadWorks(cards, btnID);
+  });
+  document.querySelector('#loadModal3').addEventListener('click', (e) => {
+    const btnID = e.target.id;
+    modal.classList.add('showModal');
+    loadWorks(cards, btnID);
+  });
+  document.querySelector('#loadModal4').addEventListener('click', (e) => {
+    const btnID = e.target.id;
+    modal.classList.add('showModal');
+    loadWorks(cards, btnID);
   });
 });
