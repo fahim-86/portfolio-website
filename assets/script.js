66510f17611b2a48512efa65d014383e944a0e63
fahim-86 <fam.fahim@yahoo.com>
2023-02-11 19:58:42 +1100

    Add JS code to store session data locally

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,26 @@ form.addEventListener('submit', (event) => {
     alertMsg.innerText = '* Please Enter Your Email Only In Lower Case';
   }
 });
+
+// data preservation inside the browser
+
+const dataTotal = {};
+document.addEventListener('keyup', () => {
+  dataTotal.name = document.querySelector('.name-area').value;
+  dataTotal.email = document.querySelector('.email-area').value;
+  dataTotal.text = document.querySelector('.text-area').value;
+  const allData = JSON.stringify(dataTotal);
+  localStorage.setItem('dataTotal', allData);
+});
+
+const storeItem = () => {
+  const parsedData = JSON.parse(localStorage.getItem('sum'));
+  const name = document.querySelector('.name-area');
+  const email = document.querySelector('.email-area');
+  const text = document.querySelector('.text-area');
+  name.value = parsedData.name;
+  email.value = parsedData.email;
+  text.value = parsedData.text;
+};
+
+window.onload = storeItem;
